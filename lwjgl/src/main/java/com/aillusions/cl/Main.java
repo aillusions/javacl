23feddc12c50af5e779611439b2ed9c214af5936
Alex Zalizniak <alex@xplace.com>
2018-01-05 13:28:11 +0200

    WIP

--- lwjgl/src/main/java/com/aillusions/cl/Main.java
+++ lwjgl/src/main/java/com/aillusions/cl/Main.java
@@ -1,5 +1,8 @@
 package com.aillusions.cl;
 
+import org.lwjgl.opencl.CL;
+import org.lwjgl.system.Configuration;
+
 /**
  * https://www.lwjgl.org/customize
  * https://github.com/LWJGL/lwjgl3
@@ -8,9 +11,24 @@ package com.aillusions.cl;
  */
 public class Main {
 
-    public static void main(String... args) {
-        //CL.create();
-        //CLPlatform.getPlatforms();
+    static {
+        Configuration.OPENCL_EXPLICIT_INIT.set(false);
+    }
 
+    public static void main(String... args) {
+
+        try {
+            try {
+                CL.create();
+            } catch (Exception e) {
+                System.out.println("CL init nok: " + e.getMessage());
+            }
+
+
+
+
+        } finally {
+            CL.destroy();
+        }
     }
 }
